0cadb9ee22faaaadeb4195dff55aab4e0707885a
mwamba <mwambajoakim@gmail.com>
2025-08-23 17:17:21 +0300

    Get the average score

---
+++ lists_py/average_grade.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def average_grade(grades=[]):
+    """Prints the average grade from
+      a list of grades.
+    """
+    total = 0
+    for num in grades:
+        total += num
+    avg = total / len(grades)
+    print(f"The average grade is {avg}")
