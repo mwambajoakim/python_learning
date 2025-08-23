eb233c05021bb84eb32eab01be75bf534a75d72b
mwamba <mwambajoakim@gmail.com>
2025-08-23 17:48:58 +0300

    Get the highest and lowest grades

---
+++ lists_py/highest_and_lowest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def high_and_low(grades=[]):
+    """Gets the gighest and lpwest grades.
+
+    args:
+         grades: List of grades.
+    """
+    lowest = grades[0]
+    highest = grades[0]
+    for grade in grades:
+        if grade < lowest:
+            lowest = grade
+        elif grade > highest:
+            highest = grade
+    print(f"Lowest grade is {lowest}")
+    print(f"Highest grade is {highest}")
+
+            
