8e7fa9923fa74350bd7a83be8ad9b299a6858eb4
mwamba <mwambajoakim@gmail.com>
2025-08-25 10:38:50 +0300

    Access the usernames

---
+++ api_response_parser/api_resp_parser.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+api_response = {
+    "status": "success",
+    "data": {
+        "users": [
+            {"id": 1, "name": "Alice", "posts": [{"title": "Hello World", "likes": 15}]},
+            {"id": 2, "name": "Bob", "posts": [{"title": "Python Tips", "likes": 32}]}
+        ],
+        "total_count": 2
+    }
+}
+
+# 1. Extract all user names
+users = api_response["data"]["users"]
+user_names = []
+for name in users:
+    print(name["name"])
+   # user_names.append(name["name"])
+
