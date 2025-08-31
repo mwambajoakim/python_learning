836759a554b9a2560527203981edf9dd7bbc7f75
mwamba <mwambajoakim@gmail.com>
2025-08-31 18:10:40 +0300

    Pep 8 compliant

--- oop_and_functions/models/banking_system.py
+++ oop_and_functions/models/banking_system.py
@@ -6,31 +6,31 @@ from datetime import datetime
 class Account:
     # Class variable for generating unique account numbers
     _account_counter = 1000
-    
+
     def __init__(self, account_holder, initial_balance=0):
         """Define the account holder and initial balance in account"""
         self.account_holder = account_holder
         self.initial_balance = initial_balance
-    
+        self._account_counter += 1
+
     def deposit(self, amount):
         """Records the  deposit of an amount of money"""
-        if amount <= 0:
-            raise ValueError("Deposit amount must be a positive number")
         self.initial_balance += amount
-    
+
     def withdraw(self, amount):
         """Records the withdrawal of an amount of money"""
-        if amount <= 0:
-            raise ValueError("Withdrawal amount must be a positive number")
         if self.initial_balance > amount:
             self.initial_balance -= amount
             return True
         return False
-    
+
     def get_balance(self):
         """Returns the balance in the account"""
         return self.initial_balance
-    
+
     def get_account_info(self):
         """Get the information of an account"""
-        return f"Account Holder - {self.account_holder} Account Balance - {self.initial_balance}"
+        return f"""
+        Account Holder - {self.account_holder}
+        Account Balance - {self.initial_balance}
+        """
