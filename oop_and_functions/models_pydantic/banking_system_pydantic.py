ba7f797d66919c908a0f2ac009103a0637e03c2a
mwamba <mwambajoakim@gmail.com>
2025-09-20 19:17:19 +0300

    Implement deposit amount: Greater than zero

--- oop_and_functions/models_pydantic/banking_system_pydantic.py
+++ oop_and_functions/models_pydantic/banking_system_pydantic.py
@@ -1,16 +1,20 @@
-from pydantic import BaseModel
-from typing import ClassVar
+from pydantic import BaseModel, Field, conint
+from typing import ClassVar, Annotated
 
-class Account:
+
+class amount_greater_than_zero(BaseModel):
+    amount : int = Field(gt=0)
+
+class Account(BaseModel):
     # Class variable for generating unique account numbers
-    _account_counter = ClassVar[int] = 1000
+    _account_counter: ClassVar[int] = 1000
     
     account_holder: str
-    initial_balance: int = 0
+    initial_balance: int = Field(default=0)
     
-    def deposit(self, amount):
-        # Your implementation here
-        pass
+    def deposit(self, amount: int):
+       validated = amount_greater_than_zero(amount=amount)
+       self.initial_balance += validated.amount
     
     def withdraw(self, amount):
         # Your implementation here  
